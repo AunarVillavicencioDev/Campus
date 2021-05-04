8332130189d6bfba5df5e6b10b9e8e68379028dc
AunarVillavicencioDev <desarrolloweb@aunarvillavicencio.edu.co>
2021-05-04 12:14:18 -0500

    moviemento de archivos

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,58 @@
+<!-- ========== Left Sidebar Start ========== -->
+<div class="vertical-menu">
+
+    <!-- LOGO -->
+    <div class="navbar-brand-box">
+        <a href="{{route('home')}}" class="logo logo-dark">
+            <span class="logo-sm">
+                <img src="{{ URL::asset('/assets/images/logo-sm.png') }}" alt="" height="22">
+            </span>
+            <span class="logo-lg">
+                <img src="{{ URL::asset('/assets/images/logo-dark.png') }}" alt="" height="20">
+            </span>
+        </a>
+
+        <a href="{{route('home')}}" class="logo logo-light">
+            <span class="logo-sm">
+                <img src="{{ URL::asset('/assets/images/logo-sm.png') }}" alt="" height="22">
+            </span>
+            <span class="logo-lg">
+                <img src="{{ URL::asset('/assets/images/logo-light.png') }}" alt="" height="20">
+            </span>
+        </a>
+    </div>
+
+    <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect vertical-menu-btn">
+        <i class="fa fa-fw fa-bars"></i>
+    </button>
+
+    <div data-simplebar class="sidebar-menu-scroll">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li class="menu-title">@lang('translation.Menu')</li>
+
+                <li>
+                    <a href="{{route('home')}}">
+                        <i class="uil-home-alt"></i><span class="badge rounded-pill bg-primary float-end">01</span>
+                        <span>@lang('translation.Dashboard')</span>
+                    </a>
+                </li>
+                
+                <li>
+                    <a href="{{route('inscripcion.index')}}">
+                        <i class="uil-user-circle"></i>
+                        <span>@lang('inscripcion.name')</span>
+                    </a>
+                </li>
+                
+                
+
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
+<!-- Left Sidebar End -->
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,31 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+
+Route::get('/config-db-refactori-dev-2021-03-29', function() {
+    $exitCode = Artisan::call('migrate:fresh');
+//    $exitCode = Artisan::call('db:seed --class=UserSeeder');
+//    $exitCode = Artisan::call('db:seed --class=PermissionSeeder');
+//    $exitCode = Artisan::call('db:seed --class=ProgramSeeder');
+//    $exitCode = Artisan::call('db:seed --class=InscriptionSeeder');
+//    $exitCode = Artisan::call('db:seed --class=CountrySeeder');
+    
+    return 'refresh db Ok';
+});
+
+Route::get('/config-clean-cache-dev-2021-03-29', function() {
+    $exitCode = Artisan::call('config:cache');
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('view:clear');
+    $exitCode = Artisan::call('route:cache');
+    return 'refresh cache Ok';
+});
